7b52c10b427b6237299d36a68e1a77b9be358030
Elven Constancio <elven.constancio04@gmail.com>
2025-10-01 09:31:03 +0700

    style: added my-support-tickets page

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -15,9 +15,17 @@ $(document).ready(function() {
             },
             bottomEnd: 'paging'
         },
+        order: [],
         paging: true,
         searching: true,
         ordering:  true,
         pageLength: 10,
+        pagingType: "simple_numbers",
+        language: {
+            paginate: {
+                next: "Next",
+                previous: "Previous"
+            }
+        },
     });
 });
