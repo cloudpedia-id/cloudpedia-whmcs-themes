b9328e28a3347fb21934fb318f7828d8fbfd6a3f
itsgitz <anggit.ginanjar.dev@gmail.com>
2025-10-22 03:45:52 +0700

    style: implement the login page on WHMCS - WIP

--- whmcs/themes/cloudpedia/js/script.js
+++ whmcs/themes/cloudpedia/js/script.js
@@ -3,6 +3,24 @@ function toggleSidebar() {
     document.getElementById("sidebar").classList.toggle("expanded");
 }
 
+// Submenu Sidebar
+document.querySelectorAll('.sidebar .has-submenu > a').forEach(function(menu){
+    menu.addEventListener('click', function(e){
+        e.preventDefault(); // biar nggak reload halaman
+        const parent = this.parentElement;
+
+        // tutup submenu lain (jika ada)
+        document.querySelectorAll('.sidebar .has-submenu').forEach(function(item){
+            if(item !== parent){
+                item.classList.remove('open');
+            }
+        });
+
+        // tambah class open pada submenu
+        parent.classList.toggle('open');
+    });
+});
+
 // Datatables
 $(document).ready(function() {
     $('#table').DataTable({
@@ -15,9 +33,17 @@ $(document).ready(function() {
             },
             bottomEnd: 'paging'
         },
+        order: [],
         paging: true,
         searching: true,
         ordering:  true,
         pageLength: 10,
+        pagingType: "simple_numbers",
+        language: {
+            paginate: {
+                next: "Next",
+                previous: "Previous"
+            }
+        },
     });
 });
